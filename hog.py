08eb6a53ecc470ae9633936b94f005ce6422aee1
B3ngineering <bengeorgeyoung@gmail.com>
2023-08-16 22:27:11 -0400

    Experimenting with hog

---
+++ hog.py
@@ -0,0 +1,40 @@
+import numpy as np
+import cv2
+
+#initialize hog
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+cv2.startWindowThread()
+
+cap = cv2.VideoCapture(0)
+
+out = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc(*'MJPG'), 15., (640, 480))
+
+while True:
+	# Frame capture
+	ret, frame = cap.read()
+	
+	frame = cv2.resize(frame, (640, 480))
+	
+	gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+	
+	#Detect people, return bounding boxes
+	boxes, weights = hog.detectMultiScale(frame, winStride=(8,8) )
+	
+	boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+	
+	for (xA, yA, xB, yB) in boxes:
+		#display detected boxes in colour picture
+		cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
+	
+	#Write output
+	out.write(frame.astype('uint8'))
+	cv2.imshow('frame',frame)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+	
+cap.release()
+out.release()
+cv2.destroyAllWindows()
+cv2.waitKey(1)
